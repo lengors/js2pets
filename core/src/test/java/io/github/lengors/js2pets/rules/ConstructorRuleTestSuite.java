e5467093f0a58f81e61cffd30e1df270884b37ed
lengors <24527258+lengors@users.noreply.github.com>
2024-09-03 13:43:51 +0000

    ✅ Restructure `ConstructorRule` tests visibility

--- core/src/test/java/io/github/lengors/js2pets/rules/ConstructorRuleTestSuite.java
+++ core/src/test/java/io/github/lengors/js2pets/rules/ConstructorRuleTestSuite.java
@@ -7,39 +7,12 @@ import org.jsonschema2pojo.rules.RuleFactory;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.sun.codemodel.JDefinedClass;
 
-/**
- * A test suite interface for providing necessary components to test the {@link ConstructorRule}. Implementations of
- * this interface should supply mock or real instances of the required schema, rule factory, JSON nodes, and super
- * constructor rules. These components are used by the {@link ConstructorRuleTestRunner} to perform unit tests.
- *
- * @author lengors
- */
-public interface ConstructorRuleTestSuite {
-  /**
-   * Retrieves the current schema being tested.
-   *
-   * @return The schema instance used in the current test context.
-   */
+interface ConstructorRuleTestSuite {
   Schema getCurrentSchema();
 
-  /**
-   * Retrieves the rule factory used to create rules for schema to Java conversion.
-   *
-   * @return The rule factory instance used in the test.
-   */
   RuleFactory getRuleFactory();
 
-  /**
-   * Retrieves the JSON node representing the part of the schema being tested.
-   *
-   * @return The JSON node being tested.
-   */
   JsonNode getNode();
 
-  /**
-   * Retrieves the rule for generating the super constructor in a defined class.
-   *
-   * @return The rule used to generate a super constructor in the test.
-   */
   Rule<JDefinedClass, JDefinedClass> getSuperConstructorRule();
 }
