8e5c35b0bab14186a1383111d4c2886f93707459
lengors <24527258+lengors@users.noreply.github.com>
2024-09-22 11:03:16 +0100

    ✅ Add tests for object rule

--- core/src/test/java/io/github/lengors/js2pets/annotators/AnnotatorUtilsTest.java
+++ core/src/test/java/io/github/lengors/js2pets/annotators/AnnotatorUtilsTest.java
@@ -7,6 +7,7 @@ import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import com.sun.codemodel.JMethod;
+import com.sun.codemodel.JType;
 
 @ExtendWith(MockitoExtension.class)
 class AnnotatorUtilsTest {
@@ -22,5 +23,19 @@ class AnnotatorUtilsTest {
     Mockito
         .verify(enhancedAnnotator, Mockito.only())
         .constructor(testConstructor);
+  }
+
+  @Test
+  void shouldCorrectlyNotifyAnnotatorWithType() {
+
+    final var testType = Mockito.mock(JType.class);
+    final var enhancedAnnotator = Mockito.mock(EnhancedAnnotator.class);
+    final var compositeAnnotator = new CompositeAnnotator(enhancedAnnotator);
+
+    AnnotatorUtils.type(compositeAnnotator, testType);
+
+    Mockito
+        .verify(enhancedAnnotator, Mockito.only())
+        .type(testType);
   }
 }
